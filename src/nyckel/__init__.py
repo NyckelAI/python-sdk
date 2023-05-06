dd0f1967af39c301741d3ef15e81b7e6720ab293
Oscar Beijbom <oscar@nyckel.com>
2023-05-06 13:15:43 -0700

    adding text classification functions

--- src/nyckel/__init__.py
+++ src/nyckel/__init__.py
@@ -1,6 +1,6 @@
 from .functions.text_classification_function import (
-    TextClassificationFunction,
-    TextClassificationSample,
-    ClassificationPrediction,
-)  # noqa: F401
+    TextClassificationFunction,  # noqa: F401
+    TextClassificationSample,  # noqa: F401
+    ClassificationPrediction,  # noqa: F401
+)
 from .auth import OAuth2Renewer  # noqa: F401
