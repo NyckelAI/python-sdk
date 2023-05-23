b0bf0590f8c9a02273d9d55b0f6e13166418bfde
Oscar Beijbom <oscar@nyckel.com>
2023-05-23 10:37:28 -0700

    support lossless image copy when pointing back to Nyckel URL

--- tests/test_image_classification_function.py
+++ tests/test_image_classification_function.py
@@ -1,23 +1,30 @@
 import time
+import os
+import pytest
+import numpy as np
 from nyckel import (
     ClassificationAnnotation,
     ClassificationLabel,
     ClassificationPrediction,
     ImageClassificationSample,
+    ImageClassificationFunction,
 )
+from nyckel.functions.classification.image_classification import ImageDecoder, ImageEncoder
 from PIL import Image
 import numpy as np
-from nyckel.functions.classification.image_classification import ImageEncoder, ImageDecoder
+from conftest import get_test_user
+
+user = get_test_user()
 
 
 def make_random_image(size=100):
     imarray = np.random.rand(size, size, 3) * 255
     img = Image.fromarray(imarray.astype("uint8")).convert("RGB")
-    return ImageEncoder().to_base64(img)
+    return ImageEncoder().image_to_base64(img)
 
 
 def test_samples(image_classification_function):
-    func = image_classification_function
+    func: ImageClassificationFunction = image_classification_function
     label = ClassificationLabel(name="Nice")
     func.create_labels([label])
     time.sleep(1)
@@ -47,7 +54,7 @@ def test_samples(image_classification_function):
 
 
 def test_end_to_end(image_classification_function):
-    func = image_classification_function
+    func: ImageClassificationFunction = image_classification_function
 
     labels_to_create = [ClassificationLabel(name="Nice"), ClassificationLabel(name="Boo")]
     func.create_labels(labels_to_create)
@@ -73,3 +80,23 @@ def test_end_to_end(image_classification_function):
     returned_samples = func.list_samples()
     assert len(samples) == len(returned_samples)
     assert isinstance(returned_samples[0], ImageClassificationSample)
+
+
+@pytest.mark.skipif(
+    user.server_url == "http://localhost:5000",
+    reason="Integrity of image on copy is only implemented for images hosted on S3.",
+)
+def test_image_integrity_on_copy(image_classification_function):
+    func: ImageClassificationFunction = image_classification_function
+    local_filepath = os.path.abspath("tests/flower.jpg")
+
+    # When Nyckel first receives an image, it resizes and recodes it.
+    first_sample_id = func.create_samples([ImageClassificationSample(data=local_filepath, external_id="v0")])[0]
+    first_sample = func.read_sample(first_sample_id)
+
+    # Here we point back to a nyckel owned url, so the images bytes are stored exactly as posted.
+    second_sample_id = func.create_samples([ImageClassificationSample(data=first_sample.data, external_id="v1")])[0]
+    second_sample = func.read_sample(second_sample_id)
+
+    decoder = ImageDecoder()
+    assert np.array_equal(np.array(decoder.to_image(first_sample.data)), np.array(decoder.to_image(second_sample.data)))
