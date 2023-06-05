9b39f58292979998aa51af505689a3e521f6acd2
Oscar Beijbom <oscar@nyckel.com>
2023-06-05 15:56:42 -0700

    checking available after function creation

--- src/nyckel/functions/classification/function_handler.py
+++ src/nyckel/functions/classification/function_handler.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import requests
+import time
 
 from nyckel import OAuth2Renewer
 from nyckel.functions.classification.classification import ClassificationFunctionURLHandler
@@ -10,14 +11,33 @@ from nyckel.request_utils import get_session_that_retries
 class ClassificationFunctionHandler:
     @staticmethod
     def create_function(name: str, function_input: str, auth: OAuth2Renewer) -> str:
+        def post_function() -> str:
+            url = f"{auth.server_url}/v1/functions"
+            response = session.post(url, json={"input": function_input, "output": "Classification", "name": name})
+            assert response.status_code == 200, f"Something went wrong when creating function: {response.text}"
+            prefixed_function_id = response.json()["id"]
+            function_id = strip_nyckel_prefix(prefixed_function_id)
+            return function_id
+
+        def hold_until_available(function_id: str) -> None:
+            # Before returning, make sure the new function is available via the API.
+            timeout_seconds = 5
+            t0 = time.time()
+            function_is_available = False
+            while not function_is_available:
+                if time.time() - t0 > timeout_seconds:
+                    raise ValueError("Something went wrong when posting labels.")
+                time.sleep(0.25)
+                url = f"{auth.server_url}/v1/functions/{function_id}"
+                response = session.get(url)
+                function_is_available = response.status_code == 200
+
         session = get_session_that_retries()
         session.headers.update({"authorization": "Bearer " + auth.token})
-        url = f"{auth.server_url}/v1/functions"
-        response = session.post(url, json={"input": function_input, "output": "Classification", "name": name})
 
-        assert response.status_code == 200, f"Something went wrong when creating function: {response.text}"
-        prefixed_function_id = response.json()["id"]
-        function_id = strip_nyckel_prefix(prefixed_function_id)
+        function_id = post_function()
+        hold_until_available(function_id)
+
         print(f"-> Created function {name} with id: {function_id}")
         return function_id
 
