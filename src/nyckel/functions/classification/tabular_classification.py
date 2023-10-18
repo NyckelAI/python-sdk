ef52768d8a69ed2fb15032d947324d6ba47f55c4
Oscar Beijbom <oscar@nyckel.com>
2023-10-18 14:30:44 -0700

    fixing internal reference to get_name

--- src/nyckel/functions/classification/tabular_classification.py
+++ src/nyckel/functions/classification/tabular_classification.py
@@ -71,7 +71,7 @@ class TabularClassificationFunction(ClassificationFunction):
         return self.__repr__()
 
     def __repr__(self) -> str:
-        status_string = f"Name: {self.get_name()}, id: {self.function_id}, url: {self.train_page}"
+        status_string = f"Name: {self.name}, id: {self.function_id}, url: {self.train_page}"
         return status_string
 
     def __call__(self, sample_data: TabularSampleData) -> ClassificationPrediction:  # type: ignore
