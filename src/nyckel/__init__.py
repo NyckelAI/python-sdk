e7205eb7c043022f0023e5126d07140cf26aa43d
Oscar Beijbom <oscar.beijbom@gmail.com>
2024-10-07 12:55:58 -0700

    Adding title, hero image and other layout improvements to PC access pages

--- src/nyckel/__init__.py
+++ src/nyckel/__init__.py
@@ -19,7 +19,13 @@ from .functions.classification.classification import (
     TextClassificationSample,  # noqa: F401
     TextSampleData,  # noqa: F401
 )
-
+from .functions.classification.factory import ClassificationFunctionFactory  # noqa: F401
+from .functions.classification.image_classification import ImageClassificationFunction  # noqa: F401
+from .functions.classification.tabular_classification import TabularClassificationFunction  # noqa: F401
+from .functions.classification.text_classification import TextClassificationFunction  # noqa: F401
+from .functions.pretrained import invoke  # noqa: F401
+from .functions.tags.image_tags import ImageTagsFunction  # noqa: F401
+from .functions.tags.tabular_tags import TabularTagsFunction  # noqa: F401
 from .functions.tags.tags import (
     ImageTagsSample,  # noqa: F401
     TabularTagsSample,  # noqa: F401
@@ -27,12 +33,5 @@ from .functions.tags.tags import (
     TagsPrediction,  # noqa: F401
     TextTagsSample,  # noqa: F401
 )
-from .image_processing import ImageResizer, ImageDecoder, ImageEncoder  # noqa: F401
-from .functions.classification.factory import ClassificationFunctionFactory  # noqa: F401
-from .functions.classification.image_classification import ImageClassificationFunction  # noqa: F401
-from .functions.classification.tabular_classification import TabularClassificationFunction  # noqa: F401
-from .functions.classification.text_classification import TextClassificationFunction  # noqa: F401
 from .functions.tags.text_tags import TextTagsFunction  # noqa: F401
-from .functions.tags.image_tags import ImageTagsFunction  # noqa: F401
-from .functions.tags.tabular_tags import TabularTagsFunction  # noqa: F401
-from .functions.pretrained import invoke  # noqa: F401
+from .image_processing import ImageDecoder, ImageEncoder, ImageResizer  # noqa: F401
--- src/nyckel/functions/pretrained.py
+++ src/nyckel/functions/pretrained.py
@@ -3,7 +3,7 @@ from typing import Dict
 from nyckel import Credentials
 
 
-def invoke(function_id: str, data: str, client_id: str, client_secret: str) -> Dict:
-    session = Credentials(client_id, client_secret).get_session()
+def invoke(function_id: str, data: str, credentials: Credentials) -> Dict:
+    session = credentials.get_session()
     endpoint = f"https://www.nyckel.com/v1/functions/{function_id}/invoke"
     return session.post(endpoint, json={"data": data}).json()
