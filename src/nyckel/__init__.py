9e04a147abd0374a90af4629752306d7a48fc8f8
Oscar Beijbom <oscar.beijbom@gmail.com>
2024-10-07 21:00:46 -0700

    fixing imports

--- src/nyckel/__init__.py
+++ src/nyckel/__init__.py
@@ -19,13 +19,6 @@ from .functions.classification.classification import (
     TextClassificationSample,  # noqa: F401
     TextSampleData,  # noqa: F401
 )
-from .functions.classification.factory import ClassificationFunctionFactory  # noqa: F401
-from .functions.classification.image_classification import ImageClassificationFunction  # noqa: F401
-from .functions.classification.tabular_classification import TabularClassificationFunction  # noqa: F401
-from .functions.classification.text_classification import TextClassificationFunction  # noqa: F401
-from .functions.pretrained import invoke  # noqa: F401
-from .functions.tags.image_tags import ImageTagsFunction  # noqa: F401
-from .functions.tags.tabular_tags import TabularTagsFunction  # noqa: F401
 from .functions.tags.tags import (
     ImageTagsSample,  # noqa: F401
     TabularTagsSample,  # noqa: F401
@@ -33,5 +26,13 @@ from .functions.tags.tags import (
     TagsPrediction,  # noqa: F401
     TextTagsSample,  # noqa: F401
 )
-from .functions.tags.text_tags import TextTagsFunction  # noqa: F401
 from .image_processing import ImageDecoder, ImageEncoder, ImageResizer  # noqa: F401
+from .functions.classification.factory import ClassificationFunctionFactory  # noqa: F401
+from .functions.classification.image_classification import ImageClassificationFunction  # noqa: F401
+from .functions.classification.tabular_classification import TabularClassificationFunction  # noqa: F401
+from .functions.classification.text_classification import TextClassificationFunction  # noqa: F401
+from .functions.pretrained import invoke  # noqa: F401
+from .functions.tags.image_tags import ImageTagsFunction  # noqa: F401
+from .functions.tags.tabular_tags import TabularTagsFunction  # noqa: F401
+
+from .functions.tags.text_tags import TextTagsFunction  # noqa: F401
